a1eb35d510767cddc10a1157d4b35343e4ad46e8
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-05-22 11:35:20 +1200

    added the first function

---
+++ meal.py
@@ -0,0 +1,16 @@
+#22/05/24 A program read the meal.db  by Japser Guo
+
+# imports
+import sqlite3
+
+
+def print_meals_by_price():
+    db = sqlite3.connect('meal.db')
+    cursor = db.cursor()
+    cursor.execute('select foods.food_name,foods.price,special_reqs.req_name,types.types_type from foods join special_reqs on foods.special_req = special_reqs.req_id join types on foods.type = types.types_id order by foods.price;')
+    result = cursor.fetchall()
+    for i in result:
+        print(f"{i[0]:<35}  {i[1]:<6}    {i[3]:<20}")
+    db.close()  # close the DB that opened
+
+print_meals_by_price()
